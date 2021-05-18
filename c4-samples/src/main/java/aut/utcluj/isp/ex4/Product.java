c005dff9e5e498e611ada9a147bd73dd3364cc7a
hetea-ioana-alina-30127 <44019689+hetea-ioana-alina-30127@users.noreply.github.com>
2021-05-18 11:53:21 +0300

    implementing ex_4 + exceptions

--- c4-samples/src/main/java/aut/utcluj/isp/ex4/Product.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex4/Product.java
@@ -6,9 +6,13 @@ package aut.utcluj.isp.ex4;
 public class Product {
     private String productId;
     private Double price;
+    private int quatity;
+
 
     public Product(String productId, Double price) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.productId = productId;
+        this.price = price;
+        this.quatity = 0;
     }
 
     public String getProductId() {
@@ -17,5 +21,19 @@ public class Product {
 
     public Double getPrice() {
         return price;
+    }
+
+    public int getQuatity() {
+        return quatity;
+    }
+
+    public void setQuatity(int quatity) {
+        this.quatity = quatity;
+    }
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        return ((Product)obj).getProductId().equals(this.productId); //To change body of generated methods, choose Tools | Templates.
     }
 }
--- c4-samples/src/main/java/aut/utcluj/isp/ex4/ProductNotFoundException.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex4/ProductNotFoundException.java
@@ -4,4 +4,9 @@ package aut.utcluj.isp.ex4;
  * @author stefan
  */
 public class ProductNotFoundException extends Exception {
+
+    public ProductNotFoundException() {
+        super("Product not found");
+    }
+    
 }
